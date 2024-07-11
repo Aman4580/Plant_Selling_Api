a60a5fa03c581bbe69a06d8a8b09efd28ec34d6a
Student Aman4580 <amanverma7124@gmail.com>
2024-07-11 22:55:25 +0530

    first commit

---
+++ frontend/src/redux/DataSlice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DataSlice = () => {
+    return (
+        <div>
+            this will store the data only 
+        </div>
+    )
+}
+
+export default DataSlice
